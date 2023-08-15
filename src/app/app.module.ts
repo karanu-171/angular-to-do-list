c0b5efe207960b70da9a31e390cd00b2a33acd19
karanu-171 <karanujohn171@gmail.com>
2023-08-15 13:13:18 +0300

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
-import { LoginComponent } from './login/login.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { MovieComponent } from './movie/movie.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+;
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    HeaderComponent,
-    HomeComponent,
-    MovieComponent
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/service/crud.service.spec.ts
+++ src/app/service/crud.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthService } from './auth.service';
+import { CrudService } from './crud.service';
 
-describe('AuthService', () => {
-  let service: AuthService;
+describe('CrudService', () => {
+  let service: CrudService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
+    service = TestBed.inject(CrudService);
   });
 
   it('should be created', () => {
